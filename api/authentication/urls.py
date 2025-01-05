fe6b8c9a4cc654d24bc0272a9e1d706105e6396a
AbduMissoum <ma_missoum@esi.dz>
2025-01-05 14:42:17 +0100

    added requirements.txt

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -4,4 +4,5 @@ from .views import CustomTokenObtain
 urlpatterns = [
         path('login/', CustomTokenObtain.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
