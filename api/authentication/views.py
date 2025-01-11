61801637fd84410ed990d22bf445102d9b12d39f
AbduMissoum <ma_missoum@esi.dz>
2025-01-11 18:07:07 +0100

    a

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -23,6 +23,7 @@ class CustomTokenObtain(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
 
+
 @signup_schema()
 @permission_classes([AllowAny])
 @api_view(['POST'])
@@ -30,36 +31,40 @@ def signup(request):
     try:
         # Validate user data
         password = generate_random_string(12)
-        user_data =request.data.copy()
-    
-        user_data['password']=password
+        user_data = request.data.copy()
+        user_data['password'] = password
 
         user_serializer = UserSerializer(data=user_data)
         if user_serializer.is_valid():
             # Save the user and retrieve the instance
             user = user_serializer.save()
 
-            # Add user_id to the request data for Agent creation
-            user_data["languages"]=','.join(user_data["languages"])
-            user_data["tenues"]=','.join(user_data["tenues"])
-            user_data["agent_fonction"]=','.join(user_data["agent_fonction"])
-            user_data['user'] = user.id
+            try:
+                # Add user_id to the request data for Agent creation
+                user_data["languages"] = ','.join(user_data.get("languages", []))
+                user_data["tenues"] = ','.join(user_data.get("tenues", []))
+                user_data["agent_fonction"] = ','.join(user_data.get("agent_fonction", []))
+                user_data['user'] = user.id
 
-            # Validate and save agent data
-            agent_serializer = AgentSerializerCreate(data=user_data)
-            if agent_serializer.is_valid():
-                agent_serializer.save()
-                sendMail(username=user.username,email=user.email,password=password)
-                return JsonResponse({"agent":agent_serializer.data,
-                                     "user":user_serializer.data}, status=201)
-            else:
-                # Delete the created user if agent creation fails
-                user.delete()
-                print("hhhhh user deleted")
-                return JsonResponse(agent_serializer.errors, status=400)
+                # Validate and save agent data
+                agent_serializer = AgentSerializerCreate(data=user_data)
+                if agent_serializer.is_valid():
+                    agent_serializer.save()
+                    sendMail(username=user.username, email=user.email, password=password)
+                    return JsonResponse({
+                        "agent": agent_serializer.data,
+                        "user": user_serializer.data
+                    }, status=201)
+                else:
+                    # Delete the created user if agent creation fails
+                    user.delete()
+                    return JsonResponse(agent_serializer.errors, status=400)
+            except Exception as e:
+                user.delete()  # Ensure user is deleted if something goes wrong in agent creation
+                return JsonResponse({"error": str(e)}, status=400)
+
         else:
             return JsonResponse(user_serializer.errors, status=400)
+
     except Exception as e:
-        if user :   
-            user.delete()
         return JsonResponse({"error": str(e)}, status=400)
