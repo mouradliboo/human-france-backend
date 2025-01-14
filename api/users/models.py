8329e4bdc6178fee0e081ce4178d03ef4bc4c618
AbduMissoum <ma_missoum@esi.dz>
2025-01-14 08:56:25 +0100

    added

--- api/users/models.py
+++ api/users/models.py
@@ -17,6 +17,7 @@ class AgentProfile(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="agent_profile")
     photo_identity = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=20, null=True, blank=True)
     nss = models.CharField(max_length=50, unique=True)
     card_number_pro = models.CharField(max_length=50, unique=True)
     security_card_photo = models.CharField(max_length=255, null=True, blank=True)
@@ -29,7 +30,7 @@ class AgentProfile(models.Model):
     experience = models.JSONField(null=True, blank=True)
     date_of_birth = models.DateField()
     place_of_birth = models.CharField(max_length=100,null=True, blank=True)
-    
+    nationality = models.CharField(max_length=100,null=True, blank=True)
     tenues = models.CharField(max_length=255, null=True, blank=True)
     languages = models.CharField(max_length=255, null=True, blank=True)
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='pending') 
