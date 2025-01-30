afb7bdc5331743fa8295afa03f784881121bf079
AbduMissoum <ma_missoum@esi.dz>
2025-01-30 15:09:44 +0100

    added search

--- api/users/views.py
+++ api/users/views.py
@@ -13,8 +13,8 @@ from drf_spectacular.utils import extend_schema,OpenApiParameter,OpenApiTypes
 import  django_filters
 from rest_framework import filters
 class AgentListFilter(django_filters.FilterSet):
-    agent_function = django_filters.CharFilter(field_name="agent_function",lookup_expr='icontains')
-    ville = django_filters.CharFilter(field_name="ville",lookup_expr='iexact')
+    agent_function = django_filters.CharFilter(field_name="agent_function",lookup_expr='iexact')
+    ville = django_filters.CharFilter(field_name="ville",lookup_expr='iregex')
     status = django_filters.CharFilter(field_name="status",lookup_expr='exact')
 
     class Meta:
