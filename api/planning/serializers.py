76ecf15046cc2978ce8555f57c620806577bc0f6
AbduMissoum <ma_missoum@esi.dz>
2025-02-11 12:13:11 +0100

    added

--- api/planning/serializers.py
+++ api/planning/serializers.py
@@ -1,11 +1,7 @@
 from .models import Planning, Ligne, Conditions
 from users.models import Clients
 from rest_framework import serializers
-class PlanningSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Planning
-        fields = '__all__'
 
 class LigneSerializer(serializers.ModelSerializer):
     
@@ -23,4 +19,12 @@ class ConditionsSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Conditions
+        fields = '__all__'
+
+
+
+class PlanningSerializer(serializers.ModelSerializer):
+    conditions = ConditionsSerializer(read_only=True)
+    class Meta:
+        model = Planning
         fields = '__all__'
