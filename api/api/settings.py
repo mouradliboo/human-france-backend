1bf33771d370096fa5e683e331c979054541a424
AbduMissoum <ma_missoum@esi.dz>
2025-02-17 11:51:35 +0100

    added

--- api/api/settings.py
+++ api/api/settings.py
@@ -207,6 +207,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
